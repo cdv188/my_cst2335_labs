2e9ff2d45bdfbccc497443fe836220f9eb221828
Chester Don Valencerina <vale0192@algonquinlive.com>
2025-06-10 12:21:15 -0400

    Week  5 lab Done

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CST2335',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
